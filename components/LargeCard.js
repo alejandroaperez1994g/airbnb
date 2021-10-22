adebb15a2eb2a95aa2ffa8ede2fd9d4c4ce7419d
Alejandro <alejandroaperez1994g@gmail.com>
2021-10-22 16:47:15 +0200

    adjusting layout

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function LargeCard({ img, title, description, buttonText }) {
   return (
-    <section className="relative py-16 cursor-pointer">
+    <section className="relative py-5 cursor-pointer">
       <div className="relative h-96 min-w-[300px]">
         <Image
           src={img}
@@ -14,10 +14,10 @@ function LargeCard({ img, title, description, buttonText }) {
       </div>
 
       <div className="absolute top-32 left-12 ">
-        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <h3 className="text-4xl mb-3 w-64 ">{title}</h3>
         <p>{description}</p>
 
-        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
+        <button className="text-sm font-semibold text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
           {buttonText}
         </button>
       </div>
